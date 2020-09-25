49b7c9e37e494a96246b6c00a819ab2ab1bd92d5
chengxz <chengxz@jiguang.cn>
2020-09-25 16:27:22 +0800

    添加mysql、kafka

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tctools
+
+pip3 install git+https://github.com/cheng-xz/tctools
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     package_data={
         'tctools':['README.srt']
     },
-    install_requires=['pymysql','pika','redis==2.10.6','redis-py-cluster==1.3.6'],
+    install_requires=['pymysql','pika','pykafka','redis==2.10.6','redis-py-cluster==1.3.6'],
     author = 'chengxz,xiaxx',
     author_email='chengxz@jiguang.cn,xiaxx@jiguang.cn',
     url='http://www.jiguang.cn',
